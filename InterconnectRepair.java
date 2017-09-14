0231d9a568aad24baac145627cc3a1f2b48fe0ff
Julian Kaeuser <jkaeus@googlemail.com>
2017-09-14 18:17:22 +0200

    added test class which may be useful as a "Main" class

---
+++ InterconnectRepair.java
@@ -0,0 +1,11 @@
+package minitask_2;
+
+/**
+ * Created by Julian Käuser on 14.09.17.
+ */
+public class InterconnectRepair {
+
+    /*
+    This class holds the minitask 2 invocation methods etc. Created mainly for testing reasons
+     */
+}
