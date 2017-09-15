14ec4486a5bc129a83edeb50f9e0d578e7537a86
theChaoS <florianprott@gmail.com>
2017-09-15 13:52:14 +0200

    Blaupause v0.1

--- InterconnectRepair.java
+++ InterconnectRepair.java
@@ -1,13 +1,18 @@
 package minitask_2;
 
+import de.tu_darmstadt.rs.MoveModulesEverywhere;
 import edu.byu.ece.rapidSmith.design.Design;
 import edu.byu.ece.rapidSmith.design.Net;
+import edu.byu.ece.rapidSmith.design.PIP;
+import edu.byu.ece.rapidSmith.design.Pin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Julian Käuser on 14.09.17.
  */
 public class InterconnectRepair {
-
+    private static final Logger logger = LoggerFactory.getLogger(MoveModulesEverywhere.class);
     /*
     This class holds the minitask 2 invocation methods etc. Created mainly for testing reasons
      */
@@ -16,7 +21,16 @@ public class InterconnectRepair {
     }
 
     public Design fixDesign(Design brokenDesign){
+        logger.info("Testz");
+        brokenDesign.getNetMap();
         return brokenDesign;
+    }
+
+    private void PinToPin (Pin aPin){
+      //  PIP aPip;
+      //  aPin.getConnectedForward();
+
+     //   aPip.getEndWireName();
     }
 
     //TODO: rückgabetyp zu was sinnvollen ändern wie PIP oder so
