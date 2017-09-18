d47a1418819fc51d5a82d9ec3d90baab1ce1a096
Julian Kaeuser <jkaeus@googlemail.com>
2017-09-18 20:24:59 +0200

    potential/kabelbaum/änderungen für Flo

--- Potential.java
+++ Potential.java
@@ -1,9 +1,11 @@
 package minitask_2;
 
+import edu.byu.ece.rapidSmith.design.Design;
 import edu.byu.ece.rapidSmith.design.PIP;
 import edu.byu.ece.rapidSmith.device.Tile;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -11,23 +13,54 @@ import java.util.HashSet;
  */
 public class Potential {
 
+        /*
+        A Map which holds all potentials of all designs known in this runtime. Make sure that everytime a
+        Design is finished with the processing, the function clearPotentials is called so that unnecessary
+        memory allocation is avoided, if the potentials are not used again.
+         */
+        static HashMap<Design, Collection<Potential>> allPotentials;
 
-    private class Potential {
 
-        public Potential(){
+        /* #####################################################
+                       constructor
+        ######################################################## */
+
+        public Potential(Design design) {
             wires = new HashSet<Integer>;
             pips = new HashSet<PIP>();
-        };
+            if (allPotentials.get(design) == null) {
+                allPotentials.put(design, new HashSet<Potential>());
 
+            }
+                allPotentials.get(design).add(this);
+        }
+
+
+
+        // ###############################################
+        //          non-static attributes
+        // ###############################################
         // all wires with this potential
         private Collection<Integer> wires;
         // the PIPs of this potential line
         private Collection<PIP> pips;
+        // all pips which can connect this potential to another potential (=fuse)
+        private Collection<PIP> adjacentPIPs;
 
         /*
-        Should work
+           Should work
          */
         private Collection<Tile> underlyingPIPTiles;
+
+
+
+
+        /* ###############################################
+                       methods
+         *################################################
+        */
+
+
 
         /**
          * Returns the wires
@@ -61,36 +94,51 @@ public class Potential {
             return this.pips;
         }
 
-        /**
+    /**
          * adds the wire to this potential
          * @param wire
          * @return
          */
-        public boolean addWire(Integer wire){
+    public boolean addWire(Integer wire){
             return (wires.add(wire));
         }
 
-        public boolean addPip(PIP pip){
-            return pips.add(pip);
+    /**
+     * Returns all PIPs which can connect this potential to another potential
+     * @return
+     */
+        public Collection<PIP> getAdjacentPIPs() {
+            return adjacentPIPs;
         }
 
 
-
-
-
-        /*
-        Checks if this wire is on this potential
-         */
-        public boolean isWireOfPotential(Integer wire){
+    /**
+     * Checks if this wire is on this potential
+     * @param wire
+     * @return
+     */
+    public boolean isWireOfPotential(Integer wire){
             if (wires.contains(wire)) return true;
             return false;
         }
 
-        /**
-         * Literally spoken: connects the two potentials electrically; this means, they are then the same potential
-         * @param other
-         */
-        public Potential fuse(Potential other){
+    /**
+     * Checks if this PIP is yet integrated in this potential (electrically: are both sides of the pip on the same potential?)
+     * @param pip
+     * @return
+     */
+    public boolean isPIPOfPotential(PIP pip){
+            if(pips.contains(pip)){
+                return true;
+            }
+            return false;
+        }
+
+    /**
+     * Literally spoken: connects the two potentials electrically; this means, they are then the same potential
+     * @param other
+     */
+    public Potential fuse(Potential other){
             for (Integer otherWire : other.getWires()){
                 wires.add(otherWire);
             }
@@ -98,31 +146,60 @@ public class Potential {
                 pips.add(otherPip);
             }
             other.clear();
+
+
+
         }
 
-        /**
-         * Removes all wires and pips
-         */
-        public void clear(){
+    /**
+     * Removes all elements from this potential
+     */
+    public void clear(){
             wires.clear();
             pips.clear();
+            adjacentPIPs.clear();
+
         }
 
-
-
-    }
 
     /**
      * Returns the potential of this wire. If no potential is defined yet, null is returned.
      * @param wire
      * @return the Potential holding this wire
      */
-    public Potential getPotential(int wire){
-        for (Potential pot : potentials){
+     public static Potential getPotentialOfWire(Design design, int wire){
+        if (allPotentials.get(design)==null){
+            return null;
+        }
+        for (Potential pot : allPotentials.get(design)){
             if (pot.isWireOfPotential(wire)){
                 return pot;
             }
         }
         return null;
+    }
+
+    /**
+     * Returns a collection of potentials (might only be of cardinality 2) which are adjacent to the given
+     * pip (=which would be fused if the pip is set).
+     * @param pip
+     * @return
+     */
+    public static Collection<Potential> getAdjacentPotentialsOfPIP(Design design, PIP pip){
+        if (allPotentials.get(design)==null){
+            return null;
+        }
+        HashSet<Potential> set = new HashSet<Potential>();
+        Potential a, b = null;
+        for (Potential p : allPotentials.get(design)){
+            if (p.getAdjacentPIPs().contains(pip)){
+                a = p;
+                set.add(a);
+            }
+            if(a!=null && !p.equals(a) && p.getAdjacentPIPs().contains(pip)){
+                b=p;
+                set.add(b);
+            }
+        }
     }
 }
